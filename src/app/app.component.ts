9c56623a09b87846b1d2393ea5e817c9cbbd5075
YASEEN-FEROZ\mdyaseenferoz <mdyaseenferoz@outlook.com>
2023-02-26 10:53:02 +0530

    typed text effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+// @ts-ignore
 import { Component } from '@angular/core';
-import { GptService } from './gpt.service'
+import { GptService } from './gpt.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,21 +12,36 @@ export class AppComponent {
   searchTerm!: String;
   gptAns= "";
   myInputText!: string;
+  typedResponse!:string;
   mySearch ={
     text: this.searchTerm,
 };
   constructor(private gptsrc: GptService) {
 
   }
+ngOnInit():void {
+
+}
+
   generateResult(title:string) {
     this.mySearch.text=title;
     this.gptsrc.post(this.mySearch).subscribe((data: any) => {
       this.gptAns=data;
       this.mySearch.text='';
       console.log(data);
+      let i = 0;
+      let intervalId = setInterval(() => {
+        this.typedResponse += this.gptAns.charAt(i);
+        i++;
+        if (i === this.gptAns.length) {
+          clearInterval(intervalId);
+        }
+      }, 50);
      });
 
 
   }
+
+
 
 }
