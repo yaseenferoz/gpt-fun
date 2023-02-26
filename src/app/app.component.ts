6189a84c07d47c7348bccf0fb39bab954e4ea006
YASEEN-FEROZ\mdyaseenferoz <mdyaseenferoz@outlook.com>
2023-02-26 12:17:08 +0530

    adding senful text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { GptService } from './gpt.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'baiganKaGPT';
+  title = 'Talk To Machine';
   searchTerm!: String;
   gptAns= "";
   myInputText!: string;
